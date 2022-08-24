927f50e921647236432aea8f768242dc46da0c83
trevor-simms <trevor-simms@outlook.com>
2022-08-24 01:51:47 -0400

    Refactored for Modularity. v0.1c

--- src/textrpg.pas
+++ src/textrpg.pas
@@ -14,38 +14,6 @@ var
     usrInput: string;
     hero, enemy: player;
 
-function isDebug: Boolean;
-begin
-    if (ParamCount > 0) and (ParamStr(1) = 'debug') then
-        isDebug := true
-    else
-        isDebug := false
-end;
-
-function isDead(val: integer): Boolean;
-begin
-    if val < 1 then
-        isDead := true
-    else
-        isDead := false
-end;
-
-function isBattleOver(val1,val2: integer): Boolean;
-begin
-    if isDead(val1) then
-    begin
-        writeln('You died, oh noes :(');
-        isBattleOver := false;
-    end
-    else if isDead(val2) then
-    begin
-        writeln('You won, good job :D');
-        isBattleOver := false;
-    end
-    else
-        isBattleOver := true;
-end;
-
 { main function }
 begin
     bRun := true;
